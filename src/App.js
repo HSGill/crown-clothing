1052be57ced7f4a0a214fabc37c935094ff5f5de
HSGill <harjotgill73@gmail.com>
2020-08-27 20:35:41 +1200

    broken homepage into smaller components, finished styling menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage.component';
 function App() {
   return (
     <div className="App">
